70fa84d917f661ca86cb652adf857791e95c7d44
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-12-06 01:07:37 +0800

    Dev: <API> added store vendor

--- backend/app/Http/Controllers/VendorController.php
+++ backend/app/Http/Controllers/VendorController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Vendor;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 class VendorController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:vendors'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'tel' => ['required', 'numeric', 'doesnt_start_with:0'],
+            'address' => ['required', 'string', 'max:255'],
+            'city' => ['required', 'string', 'max:255'],
+            'state' => ['required', 'string', 'max:255'],
+            'postcode' => ['required', 'numeric', 'max:20'],
+        ]);
+
+        Vendor::create([
+            'name' => $request->name,
+            'tel' => $request->tel,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'address' => $request->address,
+            'city' => $request->city,
+            'state' => $request->state,
+            'postcode' => $request->postcode,
+        ]);
+
+        return response()->noContent();
+    }
 }
--- backend/app/Models/Vendor.php
+++ backend/app/Models/Vendor.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Vendor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'tel',
+        'email',
+        'password',
+        'address',
+        'city',
+        'state',
+        'postcode',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
