ff2e5651f28466e5cebd24c2379b489c875ce523
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-12-05 23:44:23 +0800

    Dev: added vendors db migration

--- backend/database/migrations/2023_12_04_120527_create_vendors_table.php
+++ backend/database/migrations/2023_12_04_120527_create_vendors_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('vendors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('tel');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('postcode');
             $table->timestamps();
         });
     }
